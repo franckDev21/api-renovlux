87261b6e7c07360b62fc34eaf3b1b5f32c3fe64d
franckDev21 <tutodevphp@gmail.com>
2025-10-27 01:09:28 +0100

    deploy

---
+++ public/build/assets/input-error-CkezYEb5.js
@@ -0,0 +1,1 @@
+import{j as n}from"./app-GeuNv2U7.js";import{c as o}from"./createLucideIcon-qm2xpo58.js";function m({message:r,className:t="",...e}){return r?n.jsx("p",{...e,className:o("text-sm text-red-600 dark:text-red-400",t),children:r}):null}export{m as I};
