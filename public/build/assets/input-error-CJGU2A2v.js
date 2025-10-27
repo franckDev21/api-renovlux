a9ce8ea1204338d16498d4d0f1b77d555ab0c9de
franckDev21 <tutodevphp@gmail.com>
2025-10-27 01:07:38 +0100

    delete buld folder

--- public/build/assets/input-error-CJGU2A2v.js
+++
@@ -1,1 +0,0 @@
-import{j as n}from"./app-Dy40lgXZ.js";import{c as o}from"./createLucideIcon-R90aC8kQ.js";function m({message:r,className:t="",...e}){return r?n.jsx("p",{...e,className:o("text-sm text-red-600 dark:text-red-400",t),children:r}):null}export{m as I};
