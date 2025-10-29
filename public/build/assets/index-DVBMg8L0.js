8bf00f79d166f552e3eff6170b7a2dbb86b1936d
franckDev21 <tutodevphp@gmail.com>
2025-10-29 15:26:56 +0100

    build: préparation du déploiement
    - Construction des assets pour la production
    - Optimisation des dépendances
    - Nettoyage des fichiers de build

--- public/build/assets/index-DVBMg8L0.js
+++
@@ -1,1 +0,0 @@
-import{r as u}from"./app-m3PxGguJ.js";function o(r){const e=u.useRef({value:r,previous:r});return u.useMemo(()=>(e.current.value!==r&&(e.current.previous=e.current.value,e.current.value=r),e.current.previous),[r])}export{o as u};
