d8a6d2bad945a0df0ef620230cba4b6a41784c28
franckDev21 <tutodevphp@gmail.com>
2025-11-18 19:16:09 +0100

    build

---
+++ public/build/assets/skeleton-eiiiVRIJ.js
@@ -0,0 +1,1 @@
+import{j as o}from"./app-D5m8WAa4.js";import{c as r}from"./createLucideIcon-tfuowmrC.js";function m({className:t,...e}){return o.jsx("div",{"data-slot":"skeleton",className:r("bg-primary/10 animate-pulse rounded-md",t),...e})}export{m as S};
